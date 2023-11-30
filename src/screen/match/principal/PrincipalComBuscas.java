8737729c48e09002b44b6d794b70c07ee95b7874
Luiz Prudente <luiz.prudente147@gmail.com>
2023-11-30 13:59:16 -0300

    Inclusão do Scanner Inclusão HttpClient e HttpRequest e HttpResponse

--- src/screen/match/principal/PrincipalComBuscas.java
+++ src/screen/match/principal/PrincipalComBuscas.java
@@ -1,4 +1,25 @@
 package screen.match.principal;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 public class PrincipalComBuscas {
+    public static void main (String[] args) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual filme você deseja pesquisar? ");
+        String movieName = scanner.nextLine();
+
+        String endereco = "http://www.omdbapi.com/?t=" + movieName +"&apikey=e35ebea";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
 }
